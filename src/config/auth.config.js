6ffa14ff1f79c223bd1c5c6cfdcf1fece6811643
vesouveeous <112671008+vesouveeous@users.noreply.github.com>
2023-12-22 14:27:11 +0700

    Delete src directory

--- src/config/auth.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: process.env.SECRET_KEY,
-  jwtExpiration: 3600, // 1 hour
-  jwtRefreshExpiration: 86400, // 24 hours
-};
--- src/middleware/index.js
+++
@@ -1,8 +0,0 @@
-const authJwt = require("./authJWT.js");
-const verifyUser = require("./verifyUser.js");
-
-module.exports = {
-	authJwt,
-	verifyUser,
-};
-
